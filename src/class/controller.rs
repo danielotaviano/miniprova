c45577eeac66227f15e5ff721b16c157441c53e6
daniel <danielotavianopontes@gmail.com>
2024-04-01 20:52:06 -0300

    fix: add params validate

--- src/class/controller.rs
+++ src/class/controller.rs
@@ -17,14 +17,37 @@ pub struct CreateClassControllerParams {
     description: String,
 }
 
+impl CreateClassControllerParams {
+    pub fn validate(&self) -> Result<&Self, &'static str> {
+        if self.code.len() == 0 {
+            return Err("code is required");
+        }
+
+        if self.name.len() == 0 {
+            return Err("name is required");
+        }
+
+        if self.description.len() == 0 {
+            return Err("description is required");
+        }
+
+        Ok(self)
+    }
+}
+
 pub async fn create_class(
     Extension(current_user): Extension<AuthState>,
     Form(params): Form<CreateClassControllerParams>,
 ) -> impl IntoResponse {
+    let valid_params = match params.validate() {
+        Err(err) => return (StatusCode::BAD_REQUEST, err).into_response(),
+        Ok(params) => params,
+    };
+
     let class = Class::new(
-        &params.code,
-        &params.name,
-        &params.description,
+        &valid_params.code,
+        &valid_params.name,
+        &valid_params.description,
         current_user.get_user_id(),
     );
 
